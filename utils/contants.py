c55d7366e89476d4a46f877444cf76d06c51a143
Harishankar Kumar <hari01584@gmail.com>
2024-07-24 16:01:43 +0530

    Add env

--- utils/contants.py
+++ utils/contants.py
@@ -1,8 +1,8 @@
 # Description: Constants for the project
 import os
 
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
-REFRESH_TOKEN_EXPIRE_MINUTES = os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES")  # 7 days
+ACCESS_TOKEN_EXPIRE_MINUTES = 600
+REFRESH_TOKEN_EXPIRE_MINUTES = 6000
 ALGORITHM = os.getenv("ALGORITHM")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")  # should be kept secret
 JWT_REFRESH_SECRET_KEY = os.getenv("JWT_REFRESH_SECRET_KEY")  # should be kept secret
